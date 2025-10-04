3999b3e61b28e4b9add2ac21b3395986ece072a4
xuantien0201 <xtien1812@gmail.com>
2025-10-04 23:34:30 +0700

    test

--- helloworld/src/BaiTapLab7/SinhVien.java
+++ helloworld/src/BaiTapLab7/SinhVien.java
@@ -14,41 +14,4 @@ public class SinhVien {
 		System.out.println("Run info method from parent");
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public double getTax() {
-		return tax;
-	}
-
-	public void setTax(double tax) {
-		this.tax = tax;
-	}
-
-	@Override
-	public String toString() {
-		return "SinhVienIT [id=" + id + ", name=" + name + ", price=" + price + ", tax=" + tax + "]";
-	}
-
 }
